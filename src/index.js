98c35a627c48a0503bfccdea6eb5ccb06883887c
Lornakaboro <lornakaboro@gmail.com>
2023-02-24 17:02:24 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 // css files here
-import './style.css';
-import TaskList from '../modules/displayList.js';
-import * as Element from '../modules/elements.js';
+import "./style.css";
+import TaskList from "../modules/displayList.js";
+import * as Element from "../modules/elements.js";
 
 const newTask = new TaskList();
 
-const getCheckbox = (element) => ` ${
-  element.completed
-    ? `<input type="checkbox" aria-label="${element.index}" data-name="status" name="check" checked>`
-    : `<input type="checkbox" aria-label="${element.index}" data-name="status" name="check">`
-} `;
+const getCheckbox = (element) =>
+  ` ${
+    element.completed
+      ? `<input type="checkbox" aria-label="${element.index}" data-name="status" name="check" checked>`
+      : `<input type="checkbox" aria-label="${element.index}" data-name="status" name="check">`
+  } `;
 
 const getTaskItem = (element) => `<div class="list show">
                 ${getCheckbox(element)}                
                 <p class="taskdescription ${
-  element.completed ? 'strike' : ''
-}">${element.description}</p>
+                  element.completed ? "strike" : ""
+                }">${element.description}</p>
                <i class="fa fa-ellipsis-v fa-2x menu-icon" aria-label="${
-  element.index
-}"  data-name="edit"></i>
+                 element.index
+               }"  data-name="edit"></i>
           </div>`;
 
 const getDescription = (element) => `<div class="list edit">
           ${getCheckbox(element)} 
          <input type="text" class="desc" value="${
-  element.description
-}" aria-label ="${element.index}" >
+           element.description
+         }" aria-label ="${element.index}" >
          <i class="fa fa-trash-o fa-2x" aria-label="${
-  element.index
-}"  data-name="delete"></i>
+           element.index
+         }"  data-name="delete"></i>
    </div>`;
 
 const refreshList = () => {
   const list = newTask.listArray;
-  let content = '';
+  let content = "";
   if (list) {
     list.forEach((element) => {
       content += `${
@@ -46,48 +47,48 @@ const refreshList = () => {
 refreshList();
 
 // Event Listeners
-Element.addList.addEventListener('keydown', (e) => {
-  if (e.code === 'Enter') {
+Element.addList.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
     const val = Element.addList.value;
     if (val) {
       newTask.addNewTask(val);
-      Element.addList.value = '';
+      Element.addList.value = "";
       refreshList();
     }
   }
 });
 
-Element.listBody.addEventListener('click', (e) => {
-  if (e.target.nodeName === 'I') {
-    if (e.target.dataset.name === 'edit') {
+Element.listBody.addEventListener("click", (e) => {
+  if (e.target.nodeName === "I") {
+    if (e.target.dataset.name === "edit") {
       newTask.setEditTask(e.target.ariaLabel);
       refreshList();
-    } else if (e.target.dataset.name === 'delete') {
+    } else if (e.target.dataset.name === "delete") {
       newTask.removeTask(parseInt(e.target.ariaLabel, 10));
       refreshList();
     }
   }
 });
 
-Element.listBody.addEventListener('keydown', (e) => {
-  if (e.code === 'Enter') {
+Element.listBody.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
     const { target } = e;
-    if (target.tagName === 'INPUT' && target.classList.contains('desc')) {
-      const id = parseInt(target.getAttribute('aria-label'), 10);
+    if (target.tagName === "INPUT" && target.classList.contains("desc")) {
+      const id = parseInt(target.getAttribute("aria-label"), 10);
       newTask.editTask(id, target.value);
       refreshList();
     }
   }
 });
 
-Element.listBody.addEventListener('change', (e) => {
-  if (e.target.dataset.name === 'status') {
+Element.listBody.addEventListener("change", (e) => {
+  if (e.target.dataset.name === "status") {
     newTask.changeTaskCompletionStatus(parseInt(e.target.ariaLabel, 10));
     refreshList();
   }
 });
 
-Element.clear.addEventListener('click', () => {
+Element.clear.addEventListener("click", () => {
   newTask.removeCompletedTasks();
   refreshList();
 });
